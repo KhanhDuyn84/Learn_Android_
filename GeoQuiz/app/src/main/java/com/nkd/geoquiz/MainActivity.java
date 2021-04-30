889e6998928d3a5bcbc77a42b6175ee130fc18f7
KhanhDuy <khanhduyn84@gmail.com>
2021-04-30 11:22:03 +0700

    Customize the Toast

--- GeoQuiz/app/src/main/java/com/nkd/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/nkd/geoquiz/MainActivity.java
@@ -2,7 +2,9 @@ package com.nkd.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -20,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                Toast correctToast = Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT);
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.R)
+                    correctToast.setGravity(Gravity.TOP, 0 ,0);
+                correctToast.show();
             }
         });
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                Toast inCorrectToast = Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+                if(Build.VERSION.SDK_INT < Build.VERSION_CODES.R)
+                    inCorrectToast.setGravity(Gravity.TOP, 0 ,0);
+                inCorrectToast.show();
             }
         });
     }
